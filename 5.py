6a3462adf18a7d9ad38fdbfcc2b2756d178b098c
Max Madubuike <max.madubuike@gmail.com>
2017-10-15 19:14:58 -0500

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,7 @@
-#5: Simple Math
-#Write a program that prompts for two numbers.  Print the sum, difference, product, and quotient of those numbers.
+"""
+Exercise 5: Simple Math
+Write a program that prompts for two numbers.  Print the sum, difference, product and quotient of those numbers.
+"""
 
 import sys
 from basic_ops import  *
