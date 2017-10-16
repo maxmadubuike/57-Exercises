0d1e7c6774f01bfda458175667ef5e1cc488ff1c
Max Madubuike <max.madubuike@gmail.com>
2017-10-15 19:10:07 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,13 @@
-#Simple answer
+"""
+Excercise 1: Saying Hello
 
-# a = input("What is your name?")
-# print( "Hello " + a + ", nice to meet you!")
+Create a program that prompts for your name and prints a greeting using your name
+"""
 
-#Challenge 1
+a = input("What is your name?")
+print( "Hello " + a + ", nice to meet you!")
 
+# Challenges
+
+"""Write a new version of the program without using any variables"""
 print("Hello {}, nice to meet you!".format(input("What is your name?\n")))
