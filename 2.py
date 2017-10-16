25e1079e4acbc4a2f856919730b401d4594bd71d
Max Madubuike <max.madubuike@gmail.com>
2017-10-15 19:12:40 -0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,15 @@
+"""
+Excercise 2: Counting the Number of Characters
+
+Create a program that prompts for an input string and displays output that shows the input string and the number of
+characters the string contains.
+
+Constraints:
+Besure the output contains the original string.
+Use a simple output statement to construct the output.
+Use a built-in function of the programming language to determine the length of the string.
+"""
+
 def number_of_characters():
 	a = input("What is the input string?\n")
 	if a == "":
