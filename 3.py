7e0a73a3e29f12f7fd3cdf6c5c2477476d8b7d6a
Max Madubuike <max.madubuike@gmail.com>
2017-10-15 19:13:55 -0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
+"""
+Excercise 3: Printing Quotes
+Create a program that prompts for a quote and an author  Display the quotation and author.
+"""
+
 a = input("What is the quote? ")
 b = input("Who said it?  ")
 
