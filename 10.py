41f15c513f7d62c04391240898a45927c684ad62
Max Madubuike <max.madubuike@gmail.com>
2017-10-20 21:35:43 -0500

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,24 @@
+"""
+Exercise 10: Self-Checkout
+
+Create a simple self-checkout system.  Prompt for the prices and quantities of three items.  Calculate the subtotal of the items.  Then calculate the tax using a tax rate of 5.5%.  Print out the line items with the quantity and total, and then print out the subtotal, tax amount, and total.
+
+Contraints:
+
+Keep the input, processing, and output parts of your program seperate.  Collect the input, do the math operations, and string building, and then print out the outpu.
+
+Be sure you explicitly convert input to numerical data before doing calculations.
+"""
+
+def self_checkout():
+  
+  price_item_1 = input("Enter the price of item 1: ")
+  quantity_item_1 = input("Enter the quantity of item 1: ")
+  
+  price_item_2 = input("Enter the price of item 2: ")
+  quantity_item_1 = input("Enter the quantity of item 3: ")
+  
+  price_item_3 = input("Enter the price of item 3: ")
+  quantity_item_3 = input("Enter the quantity of item 3: ")
+
+  
