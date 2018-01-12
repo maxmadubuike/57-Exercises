825fd1c5e6c23972da0dacddb99079ec1541265d
Max Madubuike <max.madubuike@gmail.com>
2018-01-12 17:40:27 -0600

    Exercise 06: Retirement Calculator

--- 06.py
+++ 06.py
@@ -1,5 +1,5 @@
 """
-Excercise 06: Retirement Calculator
+Exercise 06: Retirement Calculator
 
 Create a program that determines how many years you have left until
 retirement and the year you can retire.  It should prompt for your
