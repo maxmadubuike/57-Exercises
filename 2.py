93a334388d450daab93886b29c62299c78a2df64
Max Madubuike <max.madubuike@gmail.com>
2018-01-12 14:18:43 -0600

    Exercise 02: Counting the Number of Characters
    Exercise 02: Counting the Number of Characters
    
    Create a program that prompts for an input string and displays output that shows the input string and the number of
    characters the string contains.
    
    Constraints:
    Besure the output contains the original string.
    Use a simple output statement to construct the output.
    Use a built-in function of the programming language to determine the length of the string.

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 """
-Excercise 2: Counting the Number of Characters
+Exercise 02: Counting the Number of Characters
 
 Create a program that prompts for an input string and displays output that shows the input string and the number of
 characters the string contains.
