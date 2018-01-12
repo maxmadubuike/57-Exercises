f71bd6a533a7b382ef54f39c9180643ff99c2337
Max Madubuike <max.madubuike@gmail.com>
2018-01-12 14:16:27 -0600

    Exercise 08:  Pizza Party
    Exercise 08:  Pizza Party
    
    Write a program to evenly divide pizzas.  Prompt for the number of people, the number of pizzas, 
    and the number of slices per pizza.  Ensure that the number of pieces comes out even.  Display 
    the number of pieces of pizza each person should get.  If there are leftovers, show the number 
    of leftover pieces.

--- 08.py
+++ 08.py
@@ -1,6 +1,10 @@
 """
-Exercise 8:  Pizza Party
-Write a program to evenly divide pizzas.  Prompt for the numberf of people, the number of pizzas, and the number of slices per pizza.  Ensure that the number of pieces comes out even.  Display the number of pieces of pizza each person should get.  If there are leftovers, show the number of leftover pieces.
+Exercise 08:  Pizza Party
+
+Write a program to evenly divide pizzas.  Prompt for the number of people, the number of pizzas, 
+and the number of slices per pizza.  Ensure that the number of pieces comes out even.  Display 
+the number of pieces of pizza each person should get.  If there are leftovers, show the number 
+of leftover pieces.
 """
 
 def pizza_party():
