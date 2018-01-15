f9faeb611f252c916f194529168a7bef37104d70
Max Madubuike <max.madubuike@gmail.com>
2018-01-14 18:13:22 -0600

    Exercise 38: Filtering Values
    Exercise 38: Filtering Values
    
    Create a program that prompts for a list of nubmers, sperated by spaces.
    Have the program print out a new list containing only the even nubmers.

---
+++ 38.py
@@ -0,0 +1,21 @@
+'''
+Exercise 38: Filtering Values
+
+Create a program that prompts for a list of nubmers, sperated by spaces.
+Have the program print out a new list containing only the even nubmers.
+
+'''
+
+def greeting():
+  print("Exercise 38: Filtering Values")
+  
+def numInput():
+  chosenNum = input("Enter a list of numbers, separated by spaces: ")
+  chosenNumList = []
+  for a in chosenNum:
+    if a != ' ':
+      if int(a) % 2 == 0:
+        chosenNumList.append(a)
+  print("The even numbers are {}".format(" ".join(chosenNumList)))
+    
+numInput()
