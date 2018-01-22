523a22f0d4046f9b6eace9bf406ab611161e9f5f
Max Madubuike <max.madubuike@gmail.com>
2018-01-21 22:44:28 -0600

    Update 31.py
    Problem Discussion:
    http://projects.madmax.io/57-exercises-for-programmerspython-exercises-31-karvonen-heart-rate/

--- 31.py
+++ 31.py
@@ -1,15 +1,21 @@
 '''
 Exercise 31 : Karvonen Heart Rate
-Create a program that prompts for  your age and your resting heart rate.  Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%.  Generate a table with the results.
+Create a program that prompts for  your age and your resting heart rate.  Use
+the Karvonen formula to determine the target heart rate based on a range of
+intensities from 55'%' to 95%.  Generate a table with the results.
 
 Constraints:
-Don't hard-code the percentages.  Use a loop to increment the percentages from 55 to 95.
-Ensure that the heart rate and age are entered as numbers.  Don't allow the user to continue without entering valid inputs.
+Don't hard-code the percentages.  Use a loop to increment the percentages from
+55 to 95.
+Ensure that the heart rate and age are entered as numbers.
+
+Don't allow the user to continue without entering valid inputs.
 Display the resuls in a tabular format.
 '''
 
 def greeting():
   print("Welcome to Exercise 31: Karvonen Heart Rate")
+  print("\n")
   
 def age_input():
   age_raw = input("Age:  ")
@@ -57,4 +63,8 @@ def table_creation():
     print(" {} bpm".format(TargetHeartRate))
     intensity += 5
 
-table_creation()
+def main():
+    greeting()
+    table_creation()
+    
+main()
